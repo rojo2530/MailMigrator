59e011be2f966c587f90e891b3182ae4e4cf5fea
Jose <jose@webempresa.eu>
2018-02-08 23:26:05 +0100

    validatevuee

--- views/index.view.php
+++ views/index.view.php
@@ -35,7 +35,7 @@
                             </div>
                             <div class="form-group" style="margin:20px ; width: 90%">
                                 <label for="passwordOrig">Password</label>
-                                <input name="password" class="form-control" v-validate="'required|min:6'" :class="{'input': true, 'is-danger': errors.has('password') }" type="password" placeholder="Password">
+                                <input name="password" class="form-control" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('password') }" type="password" placeholder="Password">
                                 <span v-show="errors.has('password')" class="help is-danger">{{ errors.first('password') }}</span>
                             </div>
 
