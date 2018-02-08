47fd2276eda9003d41b75622c1c41905de119a7c
Jose <jose@webempresa.eu>
2018-02-08 23:17:59 +0100

    validatevuee

--- views/index.view.php
+++ views/index.view.php
@@ -35,7 +35,7 @@
                             </div>
                             <div class="form-group" style="margin:20px ; width: 90%">
                                 <label for="passwordOrig">Password</label>
-                                <input name="password" v-validate="'required|min:6'" :class="{'input': true, 'is-danger': errors.has('form-1.password') }" type="password" placeholder="Password">
+                                <input name="password" class="form-control" v-validate="'required|min:6'" :class="{'input': true, 'is-danger': errors.has('form-1.password') }" type="password" placeholder="Password">
                                 <i v-show="errors.has('form-1.password')" class="fa fa-warning"></i>
                                 <span v-show="errors.has('form-1.password')" class="help is-danger">{{ errors.first('form-1.password') }}</span>
                             </div>
