6ce5bee28512f7202eaaecdc7ace20fbc9c91a4d
Jose <jose@webempresa.eu>
2018-02-08 21:16:26 +0100

    subida de vuejs

--- http2.live.php
+++ http2.live.php
@@ -1,6 +1,5 @@
 <?php
 include("/usr/local/cpanel/php/cpanel.php");
-include("file.css");
 $cpanel = new CPANEL();
 print $cpanel->header( "Traslada tu cuenta de correo" );
 require 'views/index.view.php';
