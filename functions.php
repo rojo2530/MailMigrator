5640cc5b4bb0ce2a2bf10b8af20b80e24aba4aad
Jose <jose@webempresa.eu>
2018-02-09 00:02:03 +0100

    validatevuee

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function dd($variable) {
+    echo '<pre>';
+    die(var_dump($variable));
+    echo '</pre>';
+}
+function getAllEmail($cpanel) {
+    $emails = $cpanel->uapi('Email', 'list_pops');
+    return $emails;
+}
