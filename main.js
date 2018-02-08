8689dfed8c03b0930d180e08367e66e112d84e2a
Jose <jose@webempresa.eu>
2018-02-08 15:47:48 +0100

    subida de vuejs

---
+++ main.js
@@ -0,0 +1,29 @@
+Vue.use(VeeValidate,{
+    fieldsBagName: 'vFields',
+    locale:'en',
+    dictionary: {
+        es: {
+            custom: {
+                email: {
+                    required: 'Your email is empty' // messages can be strings as well.
+                },
+                name: {
+                    required: 'Your name is empty'
+                }
+            }
+        }
+    }
+});
+
+var app = new Vue({
+    el: ".container",
+    data: {
+        name: ''
+    },
+    methods: {
+        validateBeforeSubmit() {
+            return this.$validator.validateAll();
+        }
+    },
+
+});
