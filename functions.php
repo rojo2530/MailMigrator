33e2e79ab0c5691989ca94131607560abaaa86cc
Jose <jose@webempresa.eu>
2018-02-09 14:56:23 +0100

    validatevuee

--- functions.php
+++ functions.php
@@ -4,17 +4,17 @@ function dd($variable) {
     die(var_dump($variable));
     echo '</pre>';
 }
-function getAllEmail($cpanel) {
+function getAllEmail(Cpanel $cpanel) {
     $emails = $cpanel->uapi('Email', 'list_pops')["cpanelresult"]["result"]["data"];
-    //$emailsValid = [];
+    $emailsActive = [];
     for ($i = 0; $i < count($emails) -1; $i++) {
         if (isActiveEmail($emails[$i])) {
             $emailsValid[] = $emails[$i]["email"];
          }
     }
-    return $emailsValid;
+    return $emailsActive;
 }
-function isActiveEmail($email) {
+function isActiveEmail(Array $email = []) {
     return $email["suspended_login"] == 0 &&
            $email["suspended_incoming"] == 0;
 }
