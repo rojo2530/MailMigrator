4709ba3715d13506e67c7742172c1068be4096c2
rojo2530 <jcantosg@gmail.com>
2018-02-13 23:52:29 +0100

    Mover ficheros y dejar todo ordenado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,7 @@
 Vue.use(VeeValidate,{
     fieldsBagName: 'vFields',
     locale:'en',
-    dictionary: {
-        es: {
-            custom: {
-                email: {
-                    required: 'Your email is empty' // messages can be strings as well.
-                },
-                name: {
-                    required: 'Your name is empty'
-                }
-            }
-        }
-    }
+
 });
 
 var app = new Vue({
