88c7522c0dc8460ab11bc929bd9f04d6a0e70028
rojo2530 <jcantosg@gmail.com>
2018-02-13 23:41:11 +0100

    Mover ficheros y dejar todo ordenado

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function dd($variable) {
     die(var_dump($variable));
     echo '</pre>';
 }
-function getAllEmail($cpanel) {
+function getAllEmail(Cpanel $cpanel) {
     $emails = $cpanel->uapi('Email', 'list_pops')["cpanelresult"]["result"]["data"];
     $emailsActive = [];
     for ($i = 0; $i < count($emails) -1; $i++) {
@@ -14,7 +14,7 @@ function getAllEmail($cpanel) {
     }
     return $emailsActive;
 }
-function isActiveEmail($email) {
+function isActiveEmail(Array $email = []) {
     return $email["suspended_login"] == 0 &&
            $email["suspended_incoming"] == 0;
 }
