90d567af62d0ab3e1d4cc3ff1c3e278ca3d27106
rojo2530 <jcantosg@gmail.com>
2018-02-13 23:48:58 +0100

    Mover ficheros y dejar todo ordenado

--- http2.live.php
+++ http2.live.php
@@ -2,7 +2,7 @@
 require 'bootstrap.php';
 $cpanel = new CPANEL();
 print $cpanel->header( "Traslada tu cuenta de correo" );
-if ($emailsActive = getAllEmail($cpanel)) {
+if ($emailsActive = Account::MailsActive()) {
     require 'views/index.view.php';
 } else {
     require 'views/noemail.view.php';
